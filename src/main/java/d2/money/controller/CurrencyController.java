374fedc850c367b1f2a82dda1fc2630bc543433f
nguyenthimai5 <mikhailthv@gmail.com>
2023-05-23 15:37:12 +0700

    feature/#2/fix_framing_code

--- src/main/java/d2/money/controller/CurrencyController.java
+++ src/main/java/d2/money/controller/CurrencyController.java
@@ -7,8 +7,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/currency/")
 public class CurrencyController {
     private final CurrencyService currencyService;
 
@@ -16,20 +18,19 @@ public class CurrencyController {
         this.currencyService = currencyService;
     }
 
-    @GetMapping("/currency")
+    @GetMapping("/index")
     public String currency(Model model) {
         model.addAttribute("listcurrency", currencyService.getAllCurrency());
         return "admin/currency";
     }
 
-
-    @GetMapping("/addCurrency")
+    @GetMapping("/add")
     public String addCurrency(@ModelAttribute CurrencyDTO currencyRequest, Model model) {
         model.addAttribute("currency", new CurrencyDTO());
         return "admin/addCurrency";
     }
 
-    @PostMapping("/createCurrency")
+    @PostMapping("/add")
     public String createCurrency(@ModelAttribute CurrencyDTO currencyRequest, Model model) {
         currencyService.save(currencyRequest);
         model.addAttribute("listcurrency", currencyService.getAllCurrency());
