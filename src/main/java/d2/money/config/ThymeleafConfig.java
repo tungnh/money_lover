6e416e3313bbfe4290a6eb257955af472c6e141f
nguyenthimai5 <mikhailthv@gmail.com>
2023-05-24 15:57:39 +0700

    feature/add_currency

--- src/main/java/d2/money/config/ThymeleafConfig.java
+++ src/main/java/d2/money/config/ThymeleafConfig.java
@@ -1,5 +1,6 @@
 package d2.money.config;
-import nz.net.ultraq.thymeleaf.LayoutDialect;
+
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
