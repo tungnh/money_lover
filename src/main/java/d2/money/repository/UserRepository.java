7befbc84f7653f52b0a0d78363ec77b1e41d4bd4
nguyenthimai5 <mikhailthv@gmail.com>
2023-05-24 14:06:42 +0700

    feature/add_currency

--- src/main/java/d2/money/repository/UserRepository.java
+++ src/main/java/d2/money/repository/UserRepository.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findUserByName (String name);
-
 }
 
 
